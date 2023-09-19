e68c47d43492b4056fcde6859339c0150ed2c9f9
profile-lab <info@profile-lab.it>
2023-09-19 12:26:50 +0200

    fix jodit textarea paste action

--- Cms/Views/layout/components/footer-tag.php
+++ Cms/Views/layout/components/footer-tag.php
@@ -21,7 +21,8 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jodit/3.7.1/jodit.min.js" integrity="sha512-nGr2E7x8rKD5JgArv0aq2U0ZQV+r4L2/4cvHFMBjoU5a84Ut/QH7V1XzZuM7drcguiGdMRdz+EVuVgi+di/pYQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jodit/3.24.7/jodit.min.js" integrity="sha512-W8OsAH1/u8SZ4Zx8favP2fRUx2X4rYdfrSLVwsOchE0ngWJoP99J0iRJSuNKiFkAIamWtpE2DGIaKL7xfreJYw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 <script src="<?= '/assets/lc-admin-assets/js/jquery.dm-uploader.min.js' ?>"></script>
 <script src="<?= '/assets/lc-admin-assets/js/selectize.min.js' ?>"></script>
 <script src="<?= '/assets/lc-admin-assets/js/script.js' ?>"></script>
--- Cms/Views/layout/components/header-tag.php
+++ Cms/Views/layout/components/header-tag.php
@@ -6,7 +6,7 @@
 
 */ ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha512-UyNhw5RNpQaCai2EdC+Js0QL4RlVmiq41DkmCJsRV3ZxipG2L0HhTqIf/H9Hp8ez2EnFlkBnjRGJU2stW3Lj+w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jodit/3.7.1/jodit.min.css" integrity="sha512-bOwG/3xH4Ngmg1mGk/RF693/R7REzxutxgwizuFpi5ecOgF6yEZ43X2yRQk1TH1GyH2DXgFTUrwf5Z5r/7LZnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jodit/3.24.7/jodit.es2018.en.min.css" integrity="sha512-+LpgB/uFVQcOMxLotyK9/l67AqyqBJMnz0rLDcSDtCZ/5h0vdGrbrWt1MnNhe6qWOJ06Qs8+qkfniww4WzoCEQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="<?= '/assets/lc-admin-assets/css/jquery.dm-uploader.min.css' ?>" />
 <?php /*
 */ ?>
