2ae14f42769520bcf00382dbc2e17dfc3f340e7d
profile-lab <info@profile-lab.it>
2023-11-05 12:06:31 +0100

    fix slide item view base

--- Web/Views/rows/gallery.php
+++ Web/Views/rows/gallery.php
@@ -4,9 +4,17 @@
         <?php if (isset($row->data_object) && count($row->data_object) > 0) { ?>
             <div class="swiper gallery_slider_cnt swiper-container">
                 <div class="swiper-wrapper">
-                    <?php foreach ($row->data_object as $slide_item) { ?>
+                    <?php foreach ($row->data_object as $slide_data) { ?>
+
                         <div class="swiper-slide">
-                            <?= view($base_view_folder . 'components/gallery-slide', ['slide_data' => $slide_item, 'format_folder' => '']) ?>
+                            <div class="gallery_slider_item">
+                                <?= h4($slide_data->title) ?>
+                                <?= single_img($slide_data->img_path, (isset($format_folder) && trim($format_folder)) ? $format_folder : '') ?>
+
+                            </div>
+                            <?php /*
+                            <?= view($base_view_folder . 'components/gallery-slide', ['slide_data' => $slide_data, 'format_folder' => '']) ?>
+                            */ ?>
                         </div>
                     <?php } ?>
                 </div>
