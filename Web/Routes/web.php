07cda50e7b59ce90e0b408339012839ab9582736
profile-lab <info@profile-lab.it>
2023-11-16 17:12:40 +0100

    pulizia commenti routes

--- Web/Routes/web.php
+++ Web/Routes/web.php
@@ -6,19 +6,6 @@ if (!$req->isCLI()) {
     $supportedLocales = config(App::class)->{'supportedLocales'};
     $supportedLocalesWithoutDefault = array_diff($supportedLocales, array($req->getDefaultLocale()));
 	if (in_array($uri->getSegment(1), $supportedLocalesWithoutDefault)) {
-		// 
-		// if (env('custom.has_shop') === TRUE) {
-		// 	$routes->add('{locale}/shop/empty-cart', '\Lc5\Web\Controllers\Shop\Shop::emptyCart', ['as' => $uri->getSegment(1) . 'web_shop_cart_empty']);
-		// 	$routes->add('{locale}/shop/remove-cart-row/(:any)', '\Lc5\Web\Controllers\Shop\Shop::cartRemoveRow/$1', ['as' => $uri->getSegment(1) . 'web_shop_cart_remove_row']);
-		// 	$routes->add('{locale}/shop/increment-qnt/(:any)', '\Lc5\Web\Controllers\Shop\Shop::cartIncrementQnt/$1', ['as' => $uri->getSegment(1) . 'web_shop_cart_increment_qnt']);
-		// 	$routes->add('{locale}/shop/decrement-qnt/(:any)', '\Lc5\Web\Controllers\Shop\Shop::cartDecrementQnt/$1', ['as' => $uri->getSegment(1) . 'web_shop_cart_decrement_qnt']);
-		// 	$routes->add('{locale}/shop/cart', '\Lc5\Web\Controllers\Shop\Shop::carrello', ['as' => $uri->getSegment(1) . 'web_shop_cart']);
-		// 	// 
-		// 	$routes->add('{locale}/shop/product/(:segment)/(:segment)', '\Lc5\Web\Controllers\Shop\Shop::detail/$1/$2', ['as' => $uri->getSegment(1) . 'web_shop_detail_model']);
-		// 	$routes->add('{locale}/shop/product/(:segment)', '\Lc5\Web\Controllers\Shop\Shop::detail/$1', ['as' => $uri->getSegment(1) . 'web_shop_detail']);
-		// 	$routes->add('{locale}/shop/(:segment)', '\Lc5\Web\Controllers\Shop\Shop::index/$1', ['as' => $uri->getSegment(1) . 'web_shop_category']);
-		// 	$routes->add('{locale}/shop', '\Lc5\Web\Controllers\Shop\Shop::index', ['as' => $uri->getSegment(1) . 'web_shop_home']);
-		// }
 		//
 		$routes->add('{locale}/archive/(:segment)/(:segment)/', '\Lc5\Web\Controllers\Posts::post/$1/$2', ['as' => $uri->getSegment(1) . 'web_posts_single']);
 		$routes->add('{locale}/archive/(:segment)', '\Lc5\Web\Controllers\Posts::index/$1', ['as' => $uri->getSegment(1) . 'web_posts_archive']);
@@ -43,31 +30,7 @@ $routes->match(['get'], 'logout', '\Lc5\Web\Controllers\Users\UserDashboard::log
 
 
 $routes->group('user-zone', ['namespace' => '\Lc5\Web\Controllers\Users', 'filter' => 'web_users_auth'], function ($routes) {
-    // $routes->group('api', function ($routes) {
-    //     $routes->match(['get', 'post'], 'set-video-status/(:num)', 'Api\Lezioni::lezioneVista/$1', ['as' => 'web_api_lezione_vista']);
-    // });
-    // $routes->match(['get', 'post'], 'create-profile', 'UserDashboard::createUserProfile', ['as' => 'web_create_user_profile']);
-    // $routes->add('set-user-profile', 'UserDashboard::setUserProfile', ['as' => 'web_set_user_profile']);
-    // $routes->add('user-switch', 'UserDashboard::userProfileSwitch', ['as' => 'web_switch_user_profile']);
-    // // 
-    // $routes->add('buy-subscription', 'UserSubscriptions::selectSubscription', ['as' => 'web_buy_subscription']);
-    // $routes->add('buy-subscription/pay/(:any)/(:any)', 'UserSubscriptions::paySubscription/$1/$2', ['as' => 'web_pay_subscription']);
-    // $routes->add('buy-subscription/ok', 'UserSubscriptions::paySubscriptionOk', ['as' => 'web_pay_subscription_ok']);
-
-
-    // // 
-    // $routes->add('quiz-results/(:any)/(:any)/(:any)/(:any)/(:any)/(:any)', 'Corsi::quizFatto/$1/$2/$3/$4/$5/$6', ['as' => 'web_lezione_quiz_fatto']);
-    // $routes->match(['get', 'post'], 'quiz/(:any)/(:any)/(:any)/(:any)/(:any)', 'Corsi::quizDaFare/$1/$2/$3/$4/$5', ['as' => 'web_lezione_quiz_da_fare']);
-    // $routes->add('lezione/(:any)/(:any)/(:any)/(:any)', 'Corsi::dettaglioLezione/$1/$2/$3/$4', ['as' => 'web_dettaglio_corso_lezione']);
-    // $routes->add('corso-dett/(:any)/(:any)/(:any)', 'Corsi::dettaglioCorso/$1/$2/$3', ['as' => 'web_dettaglio_corso']);
-
-    // $routes->add('corso/(:any)/anno/(:any)', 'Corsi::anno/$1/$2', ['as' => 'web_corsi_anno']);
-    // // $routes->add( 'corsi/(:any)', 'Corsi::scool/$1', ['as' => 'web_corsi_scool']);
-    // $routes->add('corsi/(:any)', 'Corsi::index/$1', ['as' => 'web_corsi_scuola']);
-    // $routes->add('corsi', 'Corsi::index', ['as' => 'web_corsi']);
-    // // 
-    // $routes->add('riepilogo-progressi/(:any)', 'UserDashboard::riepilogoProgressi/$1', ['as' => 'web_riepilogo_progressi']);
-    // // 
+    
     $routes->group('user-settings', function ($routes) {
         // $routes->add('membership', 'UserSettings::membershipList', ['as' => 'web_user_settings_membership']);
         // $routes->match(['get', 'post'], 'profiles/(:num)', 'UserSettings::profilesEdit/$1', ['as' => 'web_user_settings_profile_edit']);
@@ -84,22 +47,6 @@ $routes->group('user-zone', ['namespace' => '\Lc5\Web\Controllers\Users', 'filte
 $routes->match(['get', 'post'], '/payment-stripe-webhook', '\App\Controllers\App\Webhooks::paymentStripeWebhook', ['as' => 'payment_stripe_webhook']);
 
 
-
-
-
-// 
-// if (env('custom.has_shop') === TRUE) {
-// 	$routes->add('shop/empty-cart', '\Lc5\Web\Controllers\Shop\Shop::emptyCart', ['as' => 'web_shop_cart_empty']);
-// 	$routes->add('shop/remove-cart-row/(:any)', '\Lc5\Web\Controllers\Shop\Shop::cartRemoveRow/$1', ['as' => 'web_shop_cart_remove_row']);
-// 	$routes->add('shop/increment-qnt/(:any)', '\Lc5\Web\Controllers\Shop\Shop::cartIncrementQnt/$1', ['as' => 'web_shop_cart_increment_qnt']);
-// 	$routes->add('shop/decrement-qnt/(:any)', '\Lc5\Web\Controllers\Shop\Shop::cartDecrementQnt/$1', ['as' => 'web_shop_cart_decrement_qnt']);
-// 	$routes->add('shop/carrello', '\Lc5\Web\Controllers\Shop\Shop::carrello', ['as' => 'web_shop_cart']);
-// 	// 
-// 	$routes->add('shop/prodotto/(:segment)/(:segment)', '\Lc5\Web\Controllers\Shop\Shop::detail/$1/$2', ['as' => 'web_shop_detail_model']);
-// 	$routes->add('shop/prodotto/(:segment)', '\Lc5\Web\Controllers\Shop\Shop::detail/$1', ['as' => 'web_shop_detail']);
-// 	$routes->add('shop/(:segment)', '\Lc5\Web\Controllers\Shop\Shop::index/$1', ['as' => 'web_shop_category']);
-// 	$routes->add('shop', '\Lc5\Web\Controllers\Shop\Shop::index', ['as' => 'web_shop_home']);
-// }
 // 
 $routes->add('add-maintainer', '\Lc5\Web\Controllers\Pages::addMaintainer');
 //
