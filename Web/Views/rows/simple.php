958a570b6553faa2999b7711e410af184a60eea4
profile-lab <info@profile-lab.it>
2023-11-21 17:48:08 +0100

    Simple paragrafo html - new funzionalità

--- Web/Views/rows/simple.php
+++ Web/Views/rows/simple.php
@@ -1,11 +1,25 @@
 <div class="row row-<?= $row->type ?> row-<?= $row->type ?>-<?= $row->css_class ?> row-<?= $row->guid ?> <?= $row->css_class ?>  <?= $row->css_extra_class ?> <?= $row->css_color ?>">
     <div class="myIn">
-        <?= h2($row->titolo) ?>
-        <?= txt($row->testo) ?>
-        <?= parseMediaObj($row->main_img_obj) ?>
-        <?= cta($row->cta_url, $row->cta_label) ?>
         <?php if (isset($row->gallery_obj) && count($row->gallery_obj) > 0) { ?>
-            <?= view($base_view_folder . 'components/slider', ['gallery_obj' => $row->gallery_obj]) ?>
+            <div class="swiper simple_gallery swiper-container">
+                <div class="swiper-wrapper">
+                    <?php foreach ($row->gallery_obj as $gallery_img) { ?>
+                        <div class="simple_gallery_img swiper-slide">
+                            <?= single_img($gallery_img->src, (isset($row->formato_media) && trim($row->formato_media)) ? $row->formato_media : '') ?>
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="swiper-pagination"></div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            </div>
+        <?php } else { ?>
+            <?= parseMediaObj($row->main_img_obj, (isset($row->formato_media) && trim($row->formato_media)) ? $row->formato_media : '') ?>
         <?php } ?>
+        <div class="txts-row txts-row-<?= $row->type ?> txts-row-<?= $row->css_class ?>">
+            <?= h2($row->titolo) ?>
+            <?= txt($row->testo) ?>
+            <?= cta($row->cta_url, $row->cta_label) ?>
+        </div>
     </div>
 </div>
