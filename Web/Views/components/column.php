d9416db0df55083346f6814a73a91039f5389ef5
profile-lab <info@profile-lab.it>
2023-11-24 14:33:50 +0100

    row colonne senza fragmet

--- Web/Views/components/column.php
+++
@@ -1,7 +0,0 @@
-<?php if (isset($col_data) && $col_data != null) { ?>
-    <div class="row_column_item">
-        <?= h4($col_data->title) ?>
-        <?= single_img($col_data->img_path) ?>
-        <?= txt($col_data->testo) ?>
-    </div>
-<?php } ?>
--- Web/Views/rows/columns.php
+++ Web/Views/rows/columns.php
@@ -4,8 +4,14 @@
         <?= h3($row->sottotitolo) ?>
         <?php if (isset($row->data_object) && count($row->data_object) > 0) { ?>
             <div class="row_column_cnt row_column_cnt-<?= count($row->data_object) ?>">
-                <?php foreach ($row->data_object as $col_item) { ?>
-                    <?= view($base_view_folder . 'components/column', ['col_data' => $col_item]) ?>
+                <?php foreach ($row->data_object as $col_data) { ?>
+                    <?php if (isset($col_data) && $col_data != null) { ?>
+                        <div class="row_column_item">
+                            <?= h4($col_data->title) ?>
+                            <?= single_img($col_data->img_path) ?>
+                            <?= txt($col_data->testo) ?>
+                        </div>
+                    <?php } ?>
                 <?php } ?>
             </div>
         <?php } ?>
