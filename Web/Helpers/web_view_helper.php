11cd60001bcb7709a4ecf3c6b11d7ce35f6385f2
profile-lab <info@profile-lab.it>
2023-12-11 17:19:21 +0100

    controllo menu "is_current" in web_view_helper

--- Web/Helpers/web_view_helper.php
+++ Web/Helpers/web_view_helper.php
@@ -8,12 +8,12 @@ function printLangsMenu($menu, $menu_name = 'languages-menu', $has_flags = FALSE
         foreach ($menu as $lang) {
             $return_html .= '<li class="' . (($lang->is_default) ? 'default_lang' : '') . ' ' . (($lang->is_current) ? 'current_lang' : '') . ' ' . $menu_name . '-' . $lang->id . '">';
             $return_html .= '<a class="' . (($lang->is_default) ? 'default_lang' : '') . ' ' . (($lang->is_current) ? 'current_lang' : '') . ' ' . $menu_name . '-' . $lang->id . '" href="' . $lang->parameter . '">';
-            if($has_flags){
-                $return_html .= '<img src="' . site_url('/assets/img/'.$lang->label_mini.'.svg').'" alt="' . $lang->label . '" title="' . $lang->label . '" />';
-            }else{
+            if ($has_flags) {
+                $return_html .= '<img src="' . site_url('/assets/img/' . $lang->label_mini . '.svg') . '" alt="' . $lang->label . '" title="' . $lang->label . '" />';
+            } else {
                 $return_html .= '' . $lang->label . '';
             }
-            $return_html .='</a>';
+            $return_html .= '</a>';
             $return_html .= '</li>';
         }
         $return_html .= '</ul>';
@@ -24,10 +24,10 @@ function printLangsMenu($menu, $menu_name = 'languages-menu', $has_flags = FALSE
 function appLabel($label, $labels, $force_return = false)
 {
     // isset($app->app_labels)
-    $label_key = url_title(trim($label), '_', TRUE);	
+    $label_key = url_title(trim($label), '_', TRUE);
     if (isset($label_key) && trim($label_key) && isset($labels) && is_array($labels) && isset($labels[$label_key])) {
         return $labels[$label_key];
-    }else if(isset($label) && trim($label) && $force_return){
+    } else if (isset($label) && trim($label) && $force_return) {
         return $label;
     }
     return '';
@@ -67,10 +67,10 @@ function composeQueryString($key = null, $value = null)
     // 
     if ($key) {
         if (is_array($key)) {
-            foreach($key as $k => $v){
+            foreach ($key as $k => $v) {
                 $uri->addQuery($k, $v);
             }
-        }else {
+        } else {
             $__value = '';
             if ($value) {
                 $__value = $value;
@@ -100,9 +100,9 @@ function printSiteMenu($menu, $menu_name = null, $menu_id = null)
         $menu_id = $menu_name;
     }
     $current_url = current_url();
-    if($current_url ==  site_url(__locale_uri__ )){
-        
-        $current_url = null;
+    $current_url = str_replace(site_url(), '', $current_url);
+    if ($current_url ==  __locale_uri__) {
+        $current_url = '';
     }
     $return_html = '';
     if (isset($menu) && isset($menu->data) && is_array($menu->data) && count($menu->data) > 0) {
@@ -111,7 +111,7 @@ function printSiteMenu($menu, $menu_name = null, $menu_id = null)
     return $return_html;
 }
 //--------------------------------------------------
-function printChildrenMenuItems($childrens, $menu_name, $depth = 0, $current_url = null, $menu_id = null)
+function printChildrenMenuItems($childrens, $menu_name, $depth = 0, $current_url = '', $menu_id = null)
 {
     $return_html = '';
     if (isset($childrens) &&  is_array($childrens) && count($childrens) > 0) {
@@ -119,20 +119,36 @@ function printChildrenMenuItems($childrens, $menu_name, $depth = 0, $current_url
         foreach ($childrens as $c_row) {
             $has_submenu = false;
             $sub_menu_code  = '';
-            if((isset($c_row->children) && is_iterable($c_row->children))){
+            if ((isset($c_row->children) && is_iterable($c_row->children))) {
                 $has_submenu = true;
-                $sub_menu_code  =  printChildrenMenuItems($c_row->children, $menu_name, $depth + 1, $current_url) ;
+                $sub_menu_code  =  printChildrenMenuItems($c_row->children, $menu_name, $depth + 1, $current_url);
             }
-            $menu_item_url =site_url(__locale_uri__ . $c_row->parameter);
-            //  strpos($menu_item_url,  $current_url) !== false 
-            $return_html .= '<li class="'.(($has_submenu == true) ? 'has_submenu' : 'last_child_depth').'">';
-            $return_html .= '<a class="menu-item-' . $c_row->id . ' menu-item-type-' . $c_row->type . ' ' . ((isset($c_row->is_home) && $c_row->is_home == 1) ? 'link_home' : '') . ' ' .(($current_url && ($menu_item_url == $current_url)) ? 'is_current' : (($current_url == '' && (isset($c_row->is_home) && $c_row->is_home == 1) ) ? 'is_current' : '' )) . ' ' . $menu_name . '-' . $c_row->id . '" href="' . $menu_item_url . '">'. $c_row->label . '</a>';
+            $menu_item_url = (strpos($c_row->parameter, '://') !== false) ? $c_row->parameter : site_url(__locale_uri__ . $c_row->parameter);
+            $menu_item_target_attr = (strpos($c_row->parameter, '://') !== false) ? ' target="_blank" ' : '';
+            $return_html .= '<li class="' . (($has_submenu == true) ? 'has_submenu' : 'last_child_depth') . '">';
+            $return_html .= '<a ' . $menu_item_target_attr . ' class="menu-item-' . $c_row->id . ' menu-item-type-' . $c_row->type . ' ';
+            $return_html .= ((isset($c_row->is_home) && $c_row->is_home == 1) ? 'link_home' : '') . ' ';
+            $return_html .= (checkIsCurrentUrl($current_url, $c_row->parameter, $c_row->is_home)) ? ' is_current ' : '';
+            $return_html .= $menu_name . '-' . $c_row->id . '" href="' . $menu_item_url . '">' . $c_row->label . '</a>';
+            // 
             $return_html .= $sub_menu_code;
             $return_html .= '</li>';
         }
         $return_html .= '</ul>';
     }
     return $return_html;
+}
+//--------------------------------------------------
+function checkIsCurrentUrl($current_url, $url = null, $isHome = false)
+{
+    if ($current_url == '' && $isHome == 1) {
+        return true;
+    }
+    $url = ltrim( $url, '/');
+    if ($current_url != '' && $url != '' && (strpos($current_url, $url) !== false)) {
+        return true;
+    }
+    return false;
 }
 //--------------------------------------------------
 function printPostRows($entity_rows, $view_area = null)
@@ -217,7 +233,7 @@ function single_img($image_path, $format_folder = null, $css_class = '', $alt_la
 {
     $return_html = '';
     if ($img_src = single_img_url($image_path, $format_folder, $def_image)) {
-        return '<figure class="'.$css_class.'"><img src="' . $img_src . '" alt="' . $alt_label . '" /></figure>';
+        return '<figure class="' . $css_class . '"><img src="' . $img_src . '" alt="' . $alt_label . '" /></figure>';
     }
     // if (isset($image_path) && $image_path != '' && $image_path != null ) {
     //     $return_html.=''. env('custom.media_root_path') .  (($format_folder)? $format_folder .'/' : '') . $image_path .'" alt="img" />';
@@ -233,8 +249,8 @@ function single_img_url($image_path, $format_folder = null, $def_image = null)
 {
     if (isset($image_path) && $image_path != '' && $image_path != null) {
         $ext = getFileExt($image_path);
-        if($ext && $ext == 'svg'){
-            $format_folder = null; 
+        if ($ext && $ext == 'svg') {
+            $format_folder = null;
         }
         return '' . env('custom.media_root_path') .  (($format_folder) ? $format_folder . '/' : '') . $image_path . '';
     } else if ($def_image) {
@@ -253,7 +269,7 @@ function getFileExt(string $filepath)
 function embedYoutube($url = '', $auto_play = true, $container_class = 'yt_embed_cnt', $container_tag = 'div')
 {
     if ($embend_src = getYoutubeEmbed_url($url)) {
-        $return_html = '<' . $container_tag . ' class="'.$container_class.'"><iframe src="https://www.youtube.com/embed/' . $embend_src . '" frameborder="0" allow="' . (($auto_play) ? 'autoplay;' : '') . ' encrypted-media;" allowfullscreen></iframe></' . $container_tag . '>';
+        $return_html = '<' . $container_tag . ' class="' . $container_class . '"><iframe src="https://www.youtube.com/embed/' . $embend_src . '" frameborder="0" allow="' . (($auto_play) ? 'autoplay;' : '') . ' encrypted-media;" allowfullscreen></iframe></' . $container_tag . '>';
         return $return_html;
     }
     return '';
