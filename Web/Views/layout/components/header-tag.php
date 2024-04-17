69bc6db8b79d8d2b7258c457cd20c2d269d7eb23
profile-lab <info@profile-lab.it>
2024-04-17 14:22:26 +0200

    base nuovi stili e css

--- Web/Views/layout/components/header-tag.php
+++ Web/Views/layout/components/header-tag.php
@@ -9,10 +9,12 @@
 <?php if (isset($meta_og)) : ?>
 <?= $meta_og ?>
 <?php endif; ?>
+<?php /* 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap" rel="stylesheet" />
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+*/ ?>
 <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-<link rel="stylesheet" href="<?= __base_assets_folder__.'css/style-default.css' ?>" />
+<link rel="stylesheet" href="<?= __base_assets_folder__.'lc-admin-assets/frontend/style-default.css' ?>" />
--- Web/Views/layout/components/header.php
+++ Web/Views/layout/components/header.php
@@ -1,13 +1,9 @@
 <header class="site_header">
-    <div class="head_logo_cnt">
-        <div class="website_logo">
-            <a href="<?= site_url() ?>" title="Homepage">
-                <img alt="<?= $app->nome ?>" src="<?= site_url(__base_assets_folder__.'img/logo.png') ?>" />
-            </a>
-        </div>
-        <?php if (isset($app->app_claim) && trim($app->app_claim)) { ?>
-            <?= h5($app->app_claim, 'site_claim') ?>
-        <?php } ?>
+    <div class="logo">
+        <a href="<?= site_url() ?>" title="Homepage">
+            <img alt="<?= $app->nome ?>" src="<?= site_url(__base_assets_folder__ . 'lc-admin-assets/frontend/lc5_front_logo.svg') ?>" />
+        </a>
+        <?= (isset($app->app_claim) && trim($app->app_claim)) ? '<div class="site_claim">'.$app->app_claim.'</div>' : '' ?>
     </div>
     <nav class="navbar site_navbar">
         <?php if (isset($site_menus['main-menu'])) { ?>
