b1514671258ccd5bb1eeb4c17a83a490b5334184
profile-lab <info@profile-lab.it>
2024-04-29 10:27:39 +0200

    click su logo lc va su /

--- Cms/Views/layout/components/header.php
+++ Cms/Views/layout/components/header.php
@@ -1,5 +1,5 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="<?= site_url(route_to('lc_dashdoard')) ?>"><img src="/assets/lc-admin-assets/img/lc5_logo2.svg" /></a>
+    <a class="navbar-brand" href="<?= site_url() ?>"><img src="/assets/lc-admin-assets/img/lc5_logo2.svg" /></a>
     <div class="container-fluid ">
         <button class="btn btn-link btn-sm order-1 order-lg-0 ml-4" id="sidebarToggle" href="#"><span class="oi oi-menu"></span></button>
         <div class="ml-auto ml-md-0 d-flex align-items-center">
